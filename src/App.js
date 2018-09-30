102af6b61bd032366187e9b67c339c7d94328966
Donghwan Lee <colus4@gmail.com>
2018-09-30 15:17:52 +0900

    redux-pender 이용

--- src/App.js
+++ src/App.js
@@ -6,13 +6,34 @@ import * as postActions from './modules/post';
 
 class App extends Component {
 
-    loadData = () => {
+    cancelRequest = null
+
+    handleCancel = () => {
+        if(this.cancelRequest) {
+            this.cancelRequest();
+            this.cancelRequest = null;
+        }
+    }
+
+    loadData = async () => {
         const { PostActions, number } = this.props;
-        PostActions.getPost(number);
+        try {
+            const p = PostActions.getPost(number);
+            this.cancelRequest = p.cancel;
+            const response = await p;
+            console.log(response);
+        } catch(e) {
+            console.log(e);
+        }
     }
     
     componentDidMount() {
         this.loadData();
+        window.addEventListener('keyup', (e) => {
+            if(e.key === 'Escape') {
+                this.handleCancel();
+            }
+        })
     }
 
     componentDidUpdate(prevProps, prevState) {
@@ -51,8 +72,8 @@ export default connect(
     (state) => ({
         number: state.counter,
         post: state.post.data,
-        loading: state.post.pending,
-        error: state.post.error
+        loading: state.pender.pending['GET_POST'],
+        error: state.pender.failure['GET_POST']
     }),
     (dispatch) => ({
         CounterActions: bindActionCreators(counterActions, dispatch),
